618ad361a6c68b930294c36678d5dd9635bae660
Alican Ozer <alicanozer60@gmail.com>
2019-10-06 14:41:10 -0500

    completed...

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mum.waa.october2019.lab06
+## completed in ~2 hours
---
+++ src/main/java/com/students/contoller/HomeController.java
@@ -0,0 +1,20 @@
+package com.students.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+    System.out.println("HomeController.home()");
+        return "home";
+    }
+}
